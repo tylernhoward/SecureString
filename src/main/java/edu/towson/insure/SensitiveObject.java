252947b30befb4a1f5d6e08f12833f4b20ece66c
Tyler Howard <thowar4@students.towson.edu>
2018-05-04 13:45:18 -0400

    Add equals method to SensitiveObject

--- src/main/java/edu/towson/insure/SensitiveObject.java
+++ src/main/java/edu/towson/insure/SensitiveObject.java
@@ -32,6 +32,17 @@ public class SensitiveObject implements AutoCloseable {
         }
         buffer.close();
     }
+    
+    public boolean equals(CharSequence charSequence) {
+        final int length = buffer.length();
+        boolean bool = true;
+        for (int i = 0; i < length; i++) {
+            if (charSequence.charAt(i) != buffer.get(i)) {
+                bool = false;
+            }
+        }
+        return bool;
+    }
 
     @Override
     public void close() {
