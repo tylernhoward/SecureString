416d6cec74e87e68f297233883bf6b2253259a64
Tyler Howard <thowar4@students.towson.edu>
2018-05-11 11:10:39 -0400

    Update runner class for demo

--- src/main/java/edu/towson/insure/Runner.java
+++ src/main/java/edu/towson/insure/Runner.java
@@ -8,12 +8,19 @@ public class Runner {
 
         //Lifetime of credentials object only lasts until end of try/catch
         try (SecureString credentials = new SecureString()) {
-
-            credentials.append(new TestObj(123456789, "John"));
+            
+            //adds to the buffer "password"
+            credentials.append("password");
             System.out.println(credentials.read());
 
+            //equals methods for comparison
+            System.out.println(credentials.equals("wrongpassword"));
+            System.out.println(credentials.equals("password"));
+
+            //pauses program
             Scanner pause1 = new Scanner(System.in);
             pause1.next();
+
         }
         catch (Exception e) {
             e.printStackTrace();
